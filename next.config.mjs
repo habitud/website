38d323cc63d8255686b2de6194476e5632a8fbdb
Matthieu FELICITE <163021331+matthieufelicite@users.noreply.github.com>
2024-07-09 12:28:49 +0000

    update redirect ruleé

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,36 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
     rewrites() {
-
-        return {
-
-            beforeFiles: [
-                
-                {
-                    source: '/:path*',
-                    has: [
-
-                        {
-                            type: 'host',
-                            value: 'app.habitud.fr',
-                        }
-                    ],
-                    destination: '/dashboard/:path*'
-                }
+      return {
+        beforeFiles: [
+          {
+            source: '/:path*',
+            has: [
+              {
+                type: 'host',
+                value: 'app.habitud.fr',
+              }
+            ],
+            destination: '/dashboard/:path*',
+            missing: [
+              { type: 'header', key: 'next-router-prefetch' },
+              { type: 'header', key: 'purpose', value: 'prefetch' },
             ]
-        }
+          },
+          // Add this new rule to handle static assets
+          {
+            source: '/_next/:path*',
+            has: [
+              {
+                type: 'host',
+                value: 'app.habitud.fr',
+              }
+            ],
+            destination: '/_next/:path*',
+          }
+        ]
+      }
     }
   };
-
-export default nextConfig;
+  
+  export default nextConfig;
