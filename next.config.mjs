15451ff9991364826e4199cfb0fa5a2ee99b1940
Matthieu FELICITE <163021331+matthieufelicite@users.noreply.github.com>
2024-07-11 14:13:49 +0000

    Update asset path

--- next.config.mjs
+++ next.config.mjs
@@ -1,32 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    rewrites: async () => {
-      return [
-        {
-          source: '/:path*',
-          has: [
-            {
-              type: 'host',
-              value: 'app.habitud.fr',
-            }
-          ],
-          destination: '/dashboard/:path*'
-        }
-      ];
-    },
-    headers: async () => {
-      return [
-        {
-          source: '/:path*',
-          headers: [
-            {
-              key: 'Access-Control-Allow-Origin',
-              value: '*',
-            },
-          ],
-        },
-      ];
-    },
-  };
-  
-  export default nextConfig;
+  rewrites: async () => {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'app.habitud.fr',
+          }
+        ],
+        destination: '/dashboard/:path*'
+      }
+    ];
+  },
+  headers: async () => {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+        ],
+      },
+    ];
+  },
+};
+
+export default nextConfig;
