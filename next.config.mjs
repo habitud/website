dd026ca9759fb2810775a795762f7b303615c6b1
Matthieu FELICITE <163021331+matthieufelicite@users.noreply.github.com>
2024-07-12 08:57:56 +0000

    Change basePath

--- next.config.mjs
+++ next.config.mjs
@@ -1,38 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: 'https://habitud.fr',
-  
-  rewrites: async () => {
-    return {
-      beforeFiles: [
-        {
-          source: '/:path*',
-          has: [
-            {
-              type: 'host',
-              value: 'app.habitud.fr',
-            }
-          ],
-          destination: '/dashboard/:path*'
-        }
-      ]
-    };
-  },
 
-  // Ajout des en-têtes CORS si nécessaire
-  headers: async () => {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: '*',
-          },
-        ],
-      },
-    ];
-  },
+    rewrites() {
+
+        return {
+
+            beforeFiles: [
+                
+                {
+                    source: '/:path*',
+                    has: [
+
+                        {
+                            type: 'host',
+                            value: 'app.habitud.fr',
+                        }
+                    ],
+                    destination: '/dashboard/:path*'
+                }
+            ]
+        }
+    },
+    basePath: '/'
 };
 
 export default nextConfig;
