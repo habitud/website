255e5f2fbf9c3b944e09d5dbfc91fc1af0bfe0ab
Matthieu FELICITE <163021331+matthieufelicite@users.noreply.github.com>
2024-07-12 09:32:28 +0000

    Change basePath

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     async rewrites() {
-        return [
-            {
-                source: '/_next/static/:path*',
-                destination: '/_next/static/:path*',
-            },
-            {
-                source: '/:path*',
-                has: [
-                    {
-                        type: 'host',
-                        value: 'app.habitud.fr',
-                    }
-                ],
-                destination: '/dashboard/:path*'
-            }
-        ]
+        return {
+            afterFiles: [
+                {
+                    source: '/_next/static/:path*',
+                    destination: '/_next/static/:path*',
+                },
+                {
+                    source: '/logo.svg',
+                    destination: '/logo.svg',
+                },
+                {
+                    source: '/:path*',
+                    has: [
+                        {
+                            type: 'host',
+                            value: 'app.habitud.fr',
+                        }
+                    ],
+                    destination: '/dashboard/:path*'
+                }
+            ]
+        }
     }
 };
 
